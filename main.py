bbc83e6aa3c4a06ab54b7fa478d3ca96f7423869
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-12-12 13:39:36 +0400

    automatic birthday message

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-import smtplib
-import datetime as dt
+from datetime import datetime
+import pandas
 import random
-
+import smtplib
 
 MY_EMAIL = "appbreweryinfo@gmail.com"
-MY_PASSWORD = "abcd1234"
+MY_PASSWORD = "dfjhdjj246"
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-now = dt.datatime.now()
-weekday = now.weekday()
-if weekday == 1:
-    with open("quotes.txt") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
+data = pandas.read_csv("birthday.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f"Subject:Monday Motivation\n\n{quote}")
-
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday\n\n{contents}")
 
 
