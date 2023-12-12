3952f9b177a18ec740d787af61a9577690997218
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-12-12 11:22:38 +0400

    add letters mesaages

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import smtplib
+import datetime as dt
+import random
 
-my_email = "appbrewerytesting@gmail.com"
-password = "fwviehfiewcjypkr"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="appbreweryinfo@gmail.com", msg="Subject:Hello\n This is the body of my email")
-    connection.close()
+MY_EMAIL = "appbreweryinfo@gmail.com"
+MY_PASSWORD = "abcd1234"
+
+now = dt.datatime.now()
+weekday = now.weekday()
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f"Subject:Monday Motivation\n\n{quote}")
+
+
+
